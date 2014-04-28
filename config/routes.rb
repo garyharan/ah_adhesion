bb4447951cf6ce1e7d64383f3c5b63eab2840086
Gary Haran <gary.haran@gmail.com>
2014-04-28 17:42:43 -0400

    adding questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'questions/index'
+
   resource :profile, only: [:show, :edit, :update]
   resources :certifications, only: [:index, :show, :create] do
     resources :questions, only: :index
