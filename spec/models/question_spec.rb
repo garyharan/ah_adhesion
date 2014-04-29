d397547753c7686c0082310c931dbf57b47d7280
Daniel Haran <chebuctonian@gmail.com>
2014-04-28 21:29:36 -0400

    remove now useless tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,16 +8,5 @@ describe Question do
     q2 = Question.create! section_id: 2, title: "Second"
 
     q1.section.should == s1
-    s1.reload.section_questions.all.collect(&:id).should == [q1.id]
-  end
-
-  it "should belong to a subsection" do
-    s1 = Section.create! subsection_id: 1, title_fr: "Un -- sous-section"
-
-    q1 = Question.create! subsection_id: 1, title: "First"
-    q2 = Question.create! subsection_id: 2, title: "Second"
-
-    q1.subsection.should == s1
-    s1.reload.subsection_questions.all.collect(&:id).should == [q1.id]
   end
 end
