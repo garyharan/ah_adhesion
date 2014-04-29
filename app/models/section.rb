daabf75db04af1b829f471731f65d5c4a43d25be
Daniel Haran <chebuctonian@gmail.com>
2014-04-28 20:59:22 -0400

    simplify sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,5 @@
 class Section < ActiveRecord::Base
-  has_many :section_questions, class_name: "Question", primary_key: :section_id, foreign_key: :section_id
-  has_many :subsection_questions, class_name: "Question", primary_key: :subsection_id, foreign_key: :subsection_id
+  has_many :subsections, -> { order("subsection_id").where("subsection_id is not null") }, class_name: "Section", primary_key: :section_id
 
-  has_many :subsections, -> { order("subsection_id") }, class_name: "Section", foreign_key: "parent_id", primary_key: :section_id
-
-  scope :top_level, -> { where(parent_id: nil).order(:section_id) }
+  scope :top_level, -> { where(subsection_id: nil) }
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -3,12 +3,11 @@ require 'spec_helper'
 describe Section do
   before do
     @s1   = Section.create! section_id: 1, title_fr: "1"
+    @sub2 = Section.create! section_id: 1, subsection_id: 2, title_fr: "1.2"
+    @sub1 = Section.create! section_id: 1, subsection_id: 1, title_fr: "1.1"
+
     @s2   = Section.create! section_id: 2, title_fr: "2"
-
-    @sub2 = Section.create! subsection_id: 2, parent_id: 1, title_fr: "1.2"
-    @sub1 = Section.create! subsection_id: 1, parent_id: 1, title_fr: "1.1"
-
-    @sub3 = Section.create! subsection_id: 1, parent_id: 2, title_fr: "2.1"
+    @sub3 = Section.create! section_id: 2, subsection_id: 1, title_fr: "2.1"
   end
 
   it "should have a top_level scope" do
