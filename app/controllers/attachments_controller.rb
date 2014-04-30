9345cac8815a1a0edab441624e8fbc22e5b5a279
Daniel Haran <chebuctonian@gmail.com>
2014-04-29 21:37:55 -0400

    use PUT update instead of POST create; adds test

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,3 +1,16 @@
 class AttachmentsController < ApplicationController
+  def update
+    @attachment = Attachment.find(params[:id])
+    # FIXME: secure this
+    if @attachment.update_attributes(attachment_params)
+      head :ok
+    else
+      head 400
+    end
+  end
   
+  private
+    def attachment_params
+      params.permit(:url, :filename, :filepath, :filesize, :filetype)
+    end
 end
