e9546f40009e81eb4b3ba75ae814e818d74789fc
Gary Haran <gary.haran@gmail.com>
2014-05-13 19:39:00 -0400

    making sure admin user is valid

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe AdminUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be valid" do
+    AdminUser.new(email: "admin@reservert.com", password: "password").valid?.should be_true
+  end
 end
