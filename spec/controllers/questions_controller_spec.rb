3015c0212c55bea528f010bceab57fa5601af3b1
Gary Haran <gary.haran@gmail.com>
2014-11-06 16:05:32 -0500

    fixed all specs

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -7,22 +7,22 @@ describe QuestionsController do
 
     before do
       FactoryGirl.create :section, section_id: 1
-      @cert = Certification.create user_id: user.id
+      @cert = Dossier.create user_id: user.id
       sign_in user
     end
 
     it "returns http success" do
-      get 'index', certification_id: @cert.id
+      get 'index', dossier_id: @cert.id
       response.should be_success
     end
 
     it "assigns questions" do
-      get :index, certification_id: @cert.id
+      get :index, dossier_id: @cert.id
       assigns(:questions).should_not be_nil
     end
 
     it "sets a default section if none given" do
-      get :index, certification_id: @cert.id
+      get :index, dossier_id: @cert.id
       assigns(:section).should_not be_nil
     end
 
@@ -32,13 +32,13 @@ describe QuestionsController do
       end
 
       it "sets the last visited section" do
-        get :index, certification_id: @cert.id, section: 12
+        get :index, dossier_id: @cert.id, section: 12
         user.reload.last_visited_section_id.should == 12
       end
 
       it "shows the last visited section when accessing it again" do
-        get :index, certification_id: @cert.id, section: 12
-        get :index, certification_id: @cert.id
+        get :index, dossier_id: @cert.id, section: 12
+        get :index, dossier_id: @cert.id
         assigns(:section).section_id.should == 12
       end
     end
--- spec/query_objects/answers_search_spec.rb
+++ spec/query_objects/answers_search_spec.rb
@@ -2,28 +2,28 @@ require 'spec_helper'
 
 describe AnswersSearch do
   before do
-    @cert = Certification.create
-    @a1 = FactoryGirl.create(:answer, question_id: 1, certification_id: @cert.id)
-    @a2 = FactoryGirl.create(:answer, question_id: 2, certification_id: @cert.id)
-    @a3 = FactoryGirl.create(:answer, question_id: 3, certification_id: @cert.id)
-    @a4 = FactoryGirl.create(:answer, question_id: 2, certification_id: @cert.id + 1)
+    @cert = Dossier.create
+    @a1 = FactoryGirl.create(:answer, question_id: 1, dossier_id: @cert.id)
+    @a2 = FactoryGirl.create(:answer, question_id: 2, dossier_id: @cert.id)
+    @a3 = FactoryGirl.create(:answer, question_id: 3, dossier_id: @cert.id)
+    @a4 = FactoryGirl.create(:answer, question_id: 2, dossier_id: @cert.id + 1)
   end
 
   it "should return a hash of answers" do
     Answer.stub(:where) { [@a1,@a2] }
     AnswersSearch.should_receive(:new).with({1 => @a1, 2 => @a2}, @cert)
-    AnswersSearch.for_questions_and_certification(double(collect: []), @cert)
+    AnswersSearch.for_questions_and_dossier(double(collect: []), @cert)
   end
 
-  it "should find the right answers for a given certification and list of questions" do
-    answers = AnswersSearch.for_questions_and_certification([double(id: 1), double(id: 2)], @cert)
+  it "should find the right answers for a given dossier and list of questions" do
+    answers = AnswersSearch.for_questions_and_dossier([double(id: 1), double(id: 2)], @cert)
 
     answers.answer_for(double(id: 1)).should == @a1
     answers.answer_for(double(id: 2)).should == @a2
   end
 
   it "should initialize an answer if none were found" do
-    answers = AnswersSearch.for_questions_and_certification([double(id: 1), double(id: 2)], @cert)
+    answers = AnswersSearch.for_questions_and_dossier([double(id: 1), double(id: 2)], @cert)
 
     answers.answer_for(double(id: 3)).should_not be_nil
   end
