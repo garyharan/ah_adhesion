3c2a3090f96f2bd9f13ec5a4ffff0d6855436418
Gary Haran <gary.haran@gmail.com>
2014-11-20 15:18:56 -0500

    adding admin reports controller and views

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,2 +1,13 @@
-class Admin::ReportsController < ApplicationController
+class Admin::ReportsController < Admin::BaseController
+  before_filter :find_dossier
+
+  def index
+    @reports = @dossier.reports
+  end
+
+  private
+
+  def find_dossier
+    @dossier = Dossier.find params[:dossier_id]
+  end
 end
--- spec/controllers/admin/reports_controller_spec.rb
+++ spec/controllers/admin/reports_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Admin::ReportsController do
+  let(:admin) { FactoryGirl.create :user, :with_admin_rights }
+  let(:dossier) { create :dossier }
 
+  context "index" do
+    before do
+      sign_in admin
+    end
+
+    it "assigns reports" do
+      get :index, dossier_id: dossier.id
+      assigns(:reports).should_not be_nil
+    end
+  end
 end
