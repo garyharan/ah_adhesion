5f1c83e39f9bba9f0db96573c240a99a4db0fbf9
Gary Haran <gary.haran@gmail.com>
2015-03-30 20:53:22 -0400

    setting time zone

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Adhesion
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # https://devcenter.heroku.com/articles/rails-4-asset-pipeline
     config.serve_static_assets = true
